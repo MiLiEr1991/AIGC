5ce80c28aa8e88afac43114da79183749443c63a
MiLiEr1991 <745692574@qq.com>
2024-05-31 00:41:10 +0800

    添加状态查询接口

--- main.py
+++ main.py
@@ -16,9 +16,13 @@ app.register_blueprint(app_create_image_playground)
 from app.lamaRemover import app_img2img_lama_remover
 app.register_blueprint(app_img2img_lama_remover)
 
+# 查运行状态
+from app.queryRunStatus import app_query_running_status
+app.register_blueprint(app_query_running_status)
+
 if __name__== '__main__':
     import sys
-    port = 80
+    port = 6666
     if len(sys.argv) == 2:
         port = sys.argv[1]
     app.run(host='0.0.0.0', port=port, debug=False)
